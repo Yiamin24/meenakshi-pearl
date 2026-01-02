934aba7daca6c7967ee7814b89fdf2561e37bfa4
Wix Remote Machine <support@wix.com>
2026-01-02 03:25:52 +0000

    Remove blur from infrastructure section sides

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -620,9 +620,7 @@ const InfrastructureSection = ({ infrastructure }: { infrastructure: Infrastruct
               ))}
             </motion.div>
 
-            {/* Gradient Fade Edges */}
-            <div className="absolute left-0 top-0 bottom-0 w-20 md:w-32 bg-gradient-to-r from-background to-transparent z-10 pointer-events-none" />
-            <div className="absolute right-0 top-0 bottom-0 w-20 md:w-32 bg-gradient-to-l from-background to-transparent z-10 pointer-events-none" />
+
           </div>
         </div>
 
