aa7b075bad22f68b09e7ec98a2ca16178b2623a4
Wix Remote Machine <support@wix.com>
2026-01-23 05:09:43 +0000

    Fix favicon visibility

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,7 +4,7 @@ export const Head = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Favicon */}
-      <link rel="icon" href="https://static.wixstatic.com/media/cef78c_27c421fbd0dd4b05bf4d8042c41bd60b~mv2.png" type="image/png" />
+      <link rel="icon" href="https://static.wixstatic.com/media/cef78c_5f2dea6ece574e69ac1e538a3a2a203a~mv2.png" type="image/png" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
