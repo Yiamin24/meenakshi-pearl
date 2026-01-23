0338726d8d23d35a5ce2493840693c739d925f1f
Wix Remote Machine <support@wix.com>
2026-01-23 04:55:52 +0000

    Replace website favicon

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,7 +4,7 @@ export const Head = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Favicon */}
-      <link rel="icon" href="https://static.wixstatic.com/media/cef78c_1add7d96aea24cb48f164271cee5f9d9~mv2.png" type="image/png" />
+      <link rel="icon" href="https://static.wixstatic.com/media/cef78c_7bbb5bb521324cb4834152d46e79f3b6~mv2.png" type="image/png" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
